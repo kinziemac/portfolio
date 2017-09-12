a2d237e8bdb432ed5f5fd266b6e64a1a6c0c8ce7
Mackenzie Higa <mhiga@sfu.ca>
2017-09-12 12:25:19 -0700

    initial commit

---
+++ src/containers/App/App.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+import { SplashContainer } from 'containers/Splash'
+import './styles/App.css'
+
+class App extends Component {
+  render() {
+    return <SplashContainer />
+  }
+}
+
+export default App
---
+++ src/containers/Splash/SplashContainer.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+import SplashImage from 'images/SplashImage-2.jpg'
+import './styles/SplashContainer.scss'
+
+export default class SplashContainer extends Component {
+  render() {
+    return (
+      <div id="SplashContainer">
+        <img src={SplashImage} id="SplashImage" alt="night time snow sky" />
+        <div id="SplashText">
+          <h2>Welcome to React</h2>
+        </div>
+      </div>
+    )
+  }
+}
