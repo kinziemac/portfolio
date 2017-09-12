92562e23daa3ee0260123183b41f39cd36f5b332
Mackenzie Higa <mhiga@sfu.ca>
2017-09-12 12:50:41 -0700

    added some animation

--- src/containers/Splash/SplashContainer.js
+++ src/containers/Splash/SplashContainer.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react'
-import SplashImage from 'images/SplashImage-2.jpg'
+import SplashImage from 'images/SplashImage-3.jpg'
 import './styles/SplashContainer.scss'
 
 export default class SplashContainer extends Component {
   render() {
     return (
       <div id="SplashContainer">
-        <img src={SplashImage} id="SplashImage" alt="night time snow sky" />
+        {/*<img src={SplashImage} id="SplashImage" alt="night time snow sky" />*/}
+        <div id="SplashImage" />
         <div id="SplashText">
           <h2>Welcome to React</h2>
         </div>
