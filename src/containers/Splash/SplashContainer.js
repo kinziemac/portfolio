cc425a7b0cb196d8f5bf0715d762c1e538622663
kinziemac <mackenziehiga@Mackenzies-MacBook-Pro.local>
2018-03-12 20:32:58 -0700

    basic background

--- src/containers/Splash/SplashContainer.js
+++ src/containers/Splash/SplashContainer.js
@@ -1,17 +1,14 @@
-import React, { Component } from 'react'
-import SplashImage from 'images/SplashImage-3.jpg'
-import './styles/SplashContainer.scss'
+import React, { Component } from 'react';
+import './styles/SplashContainer.scss';
 
 export default class SplashContainer extends Component {
   render() {
     return (
       <div id="SplashContainer">
-        {/*<img src={SplashImage} id="SplashImage" alt="night time snow sky" />*/}
-        <div id="SplashImage" />
         <div id="SplashText">
-          <h2>Welcome to React</h2>
+          <p>Mackenzie Higa</p>
         </div>
       </div>
-    )
+    );
   }
 }
