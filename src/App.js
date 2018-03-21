8a6c2a85c28ff915a94be00e3b2bf13963cc165b
kinziemac <mackenziehiga@Mackenzies-MacBook-Pro.local>
2018-03-20 19:39:44 -0700

    fixed problem

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+import { LandingContainer } from './containers/Landing';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return <LandingContainer />;
+  }
+}
+
+export default App;
