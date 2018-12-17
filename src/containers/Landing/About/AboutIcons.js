69a85d19181f5a44eb9ba5a75234cec9da8b3c3e
Mackenzie Higa <mhiga@sfu.ca>
2018-12-17 10:13:15 -0800

    seperated about landing into different components

--- src/containers/Landing/About/AboutIcons.js
+++ src/containers/Landing/About/AboutIcons.js
@@ -8,22 +8,22 @@ export default function AboutIcons(props) {
         <div id="AboutIconsContainer">
             <a href="https://github.com/kinziemac" target="_blank" rel="noopener noreferrer">
                 <Icon path={mdiGithubCircle}
+                    className="Icons"
                     size={1.2}
-                    color="black"
                     style={{cursor: "pointer"}}
                     />
             </a>
             <a href="https://www.linkedin.com/in/mackenzie-higa/" target="_blank" rel="noopener noreferrer">
                 <Icon path={mdiLinkedinBox}
+                    className="Icons"
                     size={1.2}
-                    color="black"
                     style={{cursor: "pointer"}}
                     />
             </a>
             <a href="https://medium.com/@mackenziehiga" target="_blank" rel="noopener noreferrer">
                 <Icon path={mdiMedium}
+                    className="Icons"
                     size={1.2}
-                    color="black"
                     style={{cursor: "pointer"}}
                     />
             </a>
--- src/containers/Landing/About/AboutLanding.js
+++ src/containers/Landing/About/AboutLanding.js
@@ -1,14 +1,11 @@
 import React, { Component } from "react";
-import Icon from '@mdi/react';
-import { mdiTriangle} from '@mdi/js'
 import AboutIcons from "./AboutIcons"
+import AboutInterests from "./AboutInterests"
 import UnderlineWords from '../../Components/UnderlineWords.js'
 import "./styles/AboutLanding.scss";
 
 export default class AboutLanding extends Component {
   render() {
-    const interests = ["Hiking", "Rock Climbing", "Fitness", "International Volunteering", "Instruments"]
-    const introText = "Hello! My name is Mackenzie Higa and I’m a software engineer and web developer from Vancouver, BC. Currently, I'm in my final year at Simon Fraser University to complete my Bachelor's of Computer Science."
     const usText = "While I’ve lived in Vancouver for almost my entire life and consider myself a true Canadian, I was born in San Francisco, CA. so working in the US is not a problem for me. "
     return (
       <div className="AboutLanding" id="About">
@@ -38,28 +35,15 @@ export default class AboutLanding extends Component {
               <p>
                 {usText}
               </p>
-              <p className="ExtraInfo">
+              <p>
                 Outside of the computer science world, some of my other interests include:
-                {
-                  interests.map(interest => {
-                    return (
-                      <div key={interest}>
-                      <Icon path={mdiTriangle}
-                    size={0.5}
-                    rotate={90}
-                    color="black"
-                    />  {interest}
-                    </div>
-                    )
-                  })
-                }
+                <AboutInterests />
               </p>
             </div>
             <div id="PersonalImageContainer">
               <img id="PersonalImage" src="images/mackenzie.png" alt="Mackenzie" />
             </div>
           </div>
-
         </div>
       </div>
     );
