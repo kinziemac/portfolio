8fdb7c08bf62d9a609adb17c06dc1178bcbc38b0
Mackenzie Higa <mhiga@sfu.ca>
2018-12-21 17:26:55 -0800

    added google analytics

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import React, { Component } from "react";
 import { LandingContainer } from "./Containers/Landing";
+import ReactGA from 'react-ga';
 import "./App.css";
 
 class App extends Component {
+  initializeReactGA() {
+    ReactGA.initialize('UA-131266047-1');
+    ReactGA.pageview('/');
+  }
+
   render() {
+    this.initializeReactGA()
     return <LandingContainer />;
   }
 }
